fd9eae6a8f90eaecc6a7f8202f9002c7c944d61a
Luis Cardona <cardona-luis@outlook.com>
2024-03-16 05:42:20 -0500

    StudentReg: Reached Database

--- Controllers/RegController.cs
+++ Controllers/RegController.cs
@@ -16,12 +16,32 @@ namespace DrugFreePortal.Models
 
         // register a new student   
         [HttpPost("RegisterStudent")]
-        public IActionResult RegisterStudent(User fromData)
+        public IActionResult RegisterStudent(User dataFromUser)
         {
-            System.Console.WriteLine($"Reached backend of student registration");
-            System.Console.WriteLine($"email: {fromData.Email}");
 
-            return Json(new { Status = "Reached backend of student registration" });
+            // return json reach backend message
+            System.Console.WriteLine("Reached backend of register student");
+
+            System.Console.WriteLine($"Reached backend of student registration");
+            System.Console.WriteLine($"Account type: {AccountType.Student}");
+            System.Console.WriteLine($"First Name: {dataFromUser.FirstName}");
+            System.Console.WriteLine($"Last Name: {dataFromUser.LastName}");
+            System.Console.WriteLine($"email: {dataFromUser.Email}");
+
+            // account type student
+            dataFromUser.AccountType = AccountType.Student;
+
+            // save to database
+            if (_context.Users != null)
+            {
+                _context.Users.Add(dataFromUser);
+                _context.SaveChanges();
+            }
+
+            return Json(new { Status = "reach backend" });
+
+
+
         }
 
         [HttpPost("Login")]
