dce9ce31f12678abb7f59c266d9d6ef9ef105158
Luis Cardona <cardona-luis@outlook.com>
2024-04-13 19:52:58 -0500

    Comliance: Styles

--- wwwroot/js/adminDashboard.js
+++ wwwroot/js/adminDashboard.js
@@ -58,3 +58,107 @@ const table = document.getElementById('usersList'); // Assuming you have a table
         table.appendChild(row);
     });
 };
+
+
+
+
+window.addEventListener('load', () => {
+    fetch('/GetComplianceTypes')
+        .then(response => {
+            if (response.ok) {
+                // Handle successful response
+                return response.json();
+            } else {
+                // Handle error response
+                throw new Error('Failed to get compliance types');
+            }
+        })
+        .then(data => {
+            // Handle data from backend
+            console.log('Data from backend on load:', data);
+            // Call a function to handle the data
+            ComplianceTypeList(data);
+        })
+        .catch(error => {
+            // Handle network error
+            console.error('Network error:', error);
+        });
+});
+
+// 
+const CreateComplianceHandler = (event) => {
+    event.preventDefault();
+
+    const SchoolsSelector = document.getElementById("SchoolsSelector").value;
+    if (SchoolsSelector === "Select School") {
+        alert("Please select a school");
+        return;
+    }
+    const schoolId = document.getElementById("SchoolId").value;
+    const complianceName = document.getElementById("ComplianceName").value;
+    const complianceDetails = document.getElementById("ComplianceDetails").value;
+
+
+    const data = {
+        school: SchoolsSelector,
+        schoolId: schoolId,
+        name: complianceName,
+        details: complianceDetails
+    };
+    console.log(data);
+
+    fetch('/AddCompliance', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.ok) {
+                // Handle successful response
+                console.log('Compliance created successfully');
+                return response.json();
+            } else {
+                // Handle error response
+                console.error('Failed to create compliance');
+                throw new Error('Failed to create compliance');
+            }
+        })
+        .then(data => {
+            // Handle data from backend
+            console.log('Data from backend after submiting:', data);
+            // Call a function to handle the data
+            ComplianceTypeList(data);
+        })
+        .catch(error => {
+            // Handle network error
+            console.error('Network error:', error);
+        });
+};
+
+
+
+
+const ComplianceTypeList = (data) => {
+    console.log("data to loop", data.data);
+    const complianceTypeList = document.getElementById("ComplianceTypeList");
+    complianceTypeList.innerHTML = "";
+    data.data.forEach(compliance => {
+        console.log("compliance", compliance);
+     
+       
+        // Create table body
+        let tableBody = `
+            <tr>
+                <td>${compliance.name}</td>
+                <td>${compliance.school}</td>
+                <td>${compliance.schoolId}</td>
+                <td>${compliance.details}</td>
+            </tr>
+        `;
+
+        // Append the table body to the complianceTypeList
+        complianceTypeList.innerHTML += tableBody;
+    });
+};
