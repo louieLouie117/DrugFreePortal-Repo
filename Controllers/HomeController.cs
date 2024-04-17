5423a8757ed00cbb9867563a8872ca75c269f507
Luis Cardona <cardona-luis@outlook.com>
2024-04-17 08:29:53 -0500

    Queue: CheckIfInQueue

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -169,10 +169,21 @@ namespace DrugFreePortal.Models
         public IActionResult CheckInMethod(Queue DataFromUser)
         {
             System.Console.WriteLine("Reached backend of check in");
-
             // Get session user id
             int? UserIdInSession = HttpContext.Session.GetInt32("UserId");
             System.Console.WriteLine($"----------------UserId in session Home:CheckIn => {UserIdInSession}");
+
+
+            // Check if the user id in session is in the database
+            bool isUserInDatabase = _context.Users?.Any(u => u.UserId == UserIdInSession) ?? false;
+            System.Console.WriteLine($"----------------isUserInDatabase in Home:CheckIn => {isUserInDatabase}");
+
+            // if user is in data base return you are already in queue
+            if (isUserInDatabase)
+            {
+                return Ok(new { Status = "InQueue", Message = "You are already in the queue" });
+            }
+
 
             DataFromUser.StudentUserId = UserIdInSession?.ToString() ?? "0";
 
@@ -180,7 +191,16 @@ namespace DrugFreePortal.Models
             _context.Add(DataFromUser);
             _context.SaveChanges();
 
-            return Ok(new { QueueData = DataFromUser, Status = "Success", Message = "reached backend of Checked in" });
+
+
+
+
+
+
+            // Return the updated user data
+            return Ok(new { QueueData = DataFromUser, Status = "Success", Message = "Checked in successfully" });
+
+
         }
 
 
