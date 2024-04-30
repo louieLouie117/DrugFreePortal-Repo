505827bd84d7bdb39edb240155c2dec9fe09e54a
Luis Cardona <cardona-luis@outlook.com>
2024-04-30 11:23:17 -0500

    SubmitRecords: FrontendJsonData

--- Models/Record.cs
+++ Models/Record.cs
@@ -13,19 +13,22 @@ namespace DrugFreePortal.Models
 
         public required string LastName { get; set; }
 
-        public required string StudentId { get; set; }
+        public required int StudentId { get; set; }
 
         public required string SchoolName { get; set; }
 
         public required int SchoolId { get; set; }
 
-        public required string ComplicationType { get; set; }
+        public required int SemesterId { get; set; }
 
-        public required string Status { get; set; }
+        public required string SemesterTitle { get; set; }
+
+
+        public required string ComplianceType { get; set; }
+
+        public required string ComplianceStatus { get; set; }
 
         public required string StatusColor { get; set; }
-
-        public required string Semester { get; set; }
 
         public required string Notes { get; set; }
 
